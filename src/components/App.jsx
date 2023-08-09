20c2c556fc7ea3140cc21e64c8fb6439c9ab3298
Alla Yevlampiieva <modan1679@gmail.com>
2023-08-09 05:49:03 +0300

    add Api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { ToastContainer } from 'react-toastify';
+import PokemonForm from './PokemonForm';
+import PokemonInfo from './PokemonInfo';
+
+export default class App extends Component {
+  state = {
+    pokemonName: '',
+  };
+
+  handleFormSubmit = pokemonName => {
+    this.setState({pokemonName})
+  };
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
+        {/* <ToastContainer autoClose={3000} /> */}
+        
+      </div>
+    );
+  }
+}
+
+// Варіант коли треба на маунті щось зафечити:
+// export default class App extends Component {
+//   state = {
+//     pokemon: null,
+//     loading: false,
+//   };
+//   componentDidMount() {
+//     this.setState({ loading: true });
+//     fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+//       .then(res => res.json())
+//       .then(pokemonRes => this.setState({ pokemon: pokemonRes }))
+//       .finally(() => this.setState({ loading: false }));
+//   }
+//   render() {
+//     return (
+//       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+//         {this.state.loading && <h1>Loading...</h1>}
+//         {this.state.pokemon && (
+//           <div>
+//             Here will be Pokemon after fetch and when we will write it to state
+//             <div>{this.state.pokemon.name}</div>
+//           </div>
+//         )}
+
+//         {/* <ToastContainer autoClose={3000} /> */}
+//       </div>
+//     );
+//   }
+// }
---
+++ src/components/PokemonInfo.jsx
@@ -0,0 +1,115 @@
+import { Component } from 'react';
+import PokemonErrorView from './PokemonErrorView';
+import PokemonDataView from './PokemonDataView';
+import PokemonPendingView from './PokemonPendingView';
+
+// Варіант коду після рефакторингу:
+// 'idle' - стан простій
+// 'pending' - стан очікується виконання
+// 'resolved' - стан виконано з результатом добре
+// 'rejected' - стан відхилено
+export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+    error: null,
+    status: 'idle',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+
+    if (prevName !== nextName) {
+      console.log('Pokemon name is changed');
+
+      this.setState({ status: 'pending' });
+      fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(
+            new Error(`Sorry, there isn't pokemon ${nextName}`)
+          );
+        })
+        .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
+
+  render() {
+    const { pokemon, error, status } = this.state;
+    const { pokemonName } = this.props;
+
+    if (status === 'idle') {
+      return <div>Please input Pokemon name</div>;
+    }
+
+    if (status === 'pending') {
+      return <PokemonPendingView pokemonName={ pokemonName} />;
+    }
+
+    if (status === 'rejected') {
+      return <PokemonErrorView message={error.message} />;
+    }
+
+    if (status === 'resolved') {
+      return <PokemonDataView pokemon={pokemon} />;
+    }
+  }
+}
+
+// Варіант кода до рефакторингу:
+// export default class PokemonInfo extends Component {
+//   state = {
+//     pokemon: null,
+//     loading: false,
+//     error: null,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const prevName = prevProps.pokemonName;
+//     const nextName = this.props.pokemonName;
+
+//     if (prevName !== nextName) {
+//       console.log('Pokemon name is changed');
+
+//       this.setState({ loading: true, pokemon: null });
+//       fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+//         .then(response => {
+//           if (response.ok) {
+//             return response.json();
+//           }
+//           return Promise.reject(
+//             new Error(`Sorry, there isn't pokemon ${nextName}`)
+//           );
+//         })
+//         .then(pokemon => this.setState({ pokemon }))
+//         .catch(error => this.setState({ error }))
+//         .finally(() => this.setState({ loading: false }));
+//     }
+//   }
+
+//   render() {
+//     const { pokemon, loading, error } = this.state;
+//     const { pokemonName } = this.props;
+
+//     return (
+//       <div>
+//         {error && <h1>{error.message}</h1>}
+//         {loading && <div>Loading...</div>}
+//         {!pokemonName && <div>Please input Pokemon name</div>}
+//         {pokemon && (
+//           <div>
+//             <p>{pokemon.name}</p>
+//             <img
+//               src={pokemon.sprites.other['official-artwork'].front_default}
+//               alt={pokemon.name}
+//               width="240"
+//             />
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
